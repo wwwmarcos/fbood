b0cd0613f248e4ee918b81b7a3655023c5e36046
Aaquib Dilkash <aaquibdilkash@gmail.com>
2020-10-06 17:20:07 +0530

    Update index.js
    Hey! @eptaccio ...As most of the sites nowadays are bot-proof, this extra line of code can make your bot as real as human to most of the sites. just run this command "npm install puppeteer puppeteer-extra puppeteer-extra-plugin-stealth" through your terminal to install the puppeteer extra package...

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { config } = require('./config')
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 async function start () {
   const browser = await puppeteer.launch({
